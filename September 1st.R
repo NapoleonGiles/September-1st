7f3a1b2cacb082d28285c256a657981d01aa6227
NapoleonGiles <89228717+NapoleonGiles@users.noreply.github.com>
2021-09-03 08:46:02 -0400

    Add files via upload

---
+++ September 1st.R
@@ -0,0 +1,19 @@
+#Author: Napoleon Giles, Date: Spet.,1,2021, Purpose: Calculate ANOVA on sample data 
+
+#Load library called dplyr
+
+library(dplyr)
+
+#This is a link to the data
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+#save to a variable called path
+
+#Read the data from the website page
+
+df <- read.csv(PATH) %>% select(-X) %>% mutate(poison = factor(poison, ordered = TRUE)) 
+
+#print out data
+
+glimpse(df)
